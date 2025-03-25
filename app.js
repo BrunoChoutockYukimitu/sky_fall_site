22125c243b3a8341fae3c4a09a3eaa8591ac7b59
Bruno Choutock Yukimitu <brunochoutockyk@gmail.com>
2025-03-24 21:14:07 -0300

    .

---
+++ app.js
@@ -0,0 +1,9 @@
+/*alert("Bem vindo pessoas") */
+console.log("posso escrever aqui")
+console.log(10+2)
+console.log(10-2)
+console.log(10*2)
+console.log(10/5)
+console.log("João"+" "+"Paulo")
+alert("10"*2)
+console.log(parseFloat("10")+2)
